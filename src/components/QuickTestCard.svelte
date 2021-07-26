6e8c9b9916f10dbd834325adbdc4bde94bcc0b34
Clarence <clarencerheysalaveria@plv.edu.ph>
2021-07-26 14:50:30 +0800

    for deploying

---
+++ src/components/QuickTestCard.svelte
@@ -0,0 +1,32 @@
+<!-- Data and Script -->
+<script>
+    import QuickTestAnswerField from "./QuickTestAnswerField.svelte";
+    export let score;
+    export let correctAnswer;
+    let answerInput;
+    export let questionTitle;
+    export let lessonTitle;
+    export let correctFeedback;
+    export let incorrectFeedback;
+</script>
+
+<!-- HTML -->
+<div class="card">
+    <div class="card-header questrial text-center">
+        {lessonTitle}
+    </div>
+    <div class="card-body">
+        <h5 class="card-title questrial text-center">{questionTitle}</h5>
+        <pre class="card-text question">
+            <code class="language-java"><slot/></code>
+        </pre>
+        <div class="answer-field">
+            <QuickTestAnswerField score={score} bind:textField={answerInput} answer={correctAnswer} correctFeedback={correctFeedback} incorrectFeedback={incorrectFeedback}/>
+        </div>
+    </div>
+</div>
+
+<!-- Scope Styles -->
+<style>
+
+</style>
--- src/routes/QuickTest.svelte
+++ src/routes/QuickTest.svelte
@@ -1,28 +1,56 @@
 <!-- Data and Script -->
 <script>
     import Navbar from "../components/Navbar.svelte";
-    import QuestionCard from "../components/QuestionCard.svelte";
+    import QuickTestCard from "../components/QuickTestCard.svelte";
     // should be no more than 5
-    let questionCounter = 0;
+    let score = 0;
     let questionAnswer = {
         "questions": [
+            "__________________(\"Hello World!\");",
+
+            "System.out.println" +
+            "int prodId = 12345;\n" +
+            "String prodName = \"Flat Tops\";\n" +
+            "_____ prodPrice = 15.75;",
+
 
         ],
         "answers": [
-            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+            "System.out.println",
+            "double",
+
+        ],
+        "questionTitles": [
+            "Fill in the blanks to print out \"Hello World\"",
+
         ]
     }
     let rand = Math.floor((Math.random() * 15) + 1);
+    let questionCardProps = {
+        lessonTitle: "Lets do the Quick Test!",
+        questionTitle: rand,
+        correctAnswer: questionAnswer.answers[rand-1],
+    }
 </script>
 
 <!-- HTML -->
 <Navbar/>
-<!--<QuestionCard lessonTitle="Quick Test Question!" correctAnswer={questionAnswer[1][rand] - 1}>-->
-<!--    <pre class="question">-->
-<!--        {questionAnswer[1][rand]}-->
-<!--    </pre>-->
-<!--</QuestionCard>-->
+<div class="d-flex justify-content-center align-content-center">
+    <div class="vertical-center">
+        <QuickTestCard {...questionCardProps} score={score}>
+            <pre className="question">
+                {questionAnswer.questions[rand-1]}
+            </pre>
+        </QuickTestCard>
+    </div>
+</div>
 <!-- Scope Styles -->
 <style>
-
+    .vertical-center {
+        margin: 0;
+        position: fixed;
+        top: 50%;
+        -ms-transform: translateY(-50%);
+        transform: translateY(-50%);
+    }
 </style>
